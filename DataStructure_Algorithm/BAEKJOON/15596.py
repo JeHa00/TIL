ca004d16a1238afef7fc1741b6084d84c3a20b30
JeHa00 <rudtls0611@naver.com>
2022-08-09 23:29:59 +0900

    BAEKJOON algorithum 15596 problem

---
+++ DataStructure_Algorithm/BAEKJOON/15596.py
@@ -0,0 +1,15 @@
+
+"""
+[Problem: 15596]
+
+정수 n개가 저장되어 있는 리스트를 function이 인자로 받으면 그 리스트 성분들의 합을 구하기
+
+- 입력값: a로, 정수 n개가 저장되어 있는 배열
+- 출력값: 정수 n개의 합
+
+- 형식 제한
+    - def solve(a: list) -> int: 로 시작한다.
+"""
+
+def solve(a: list) -> int:
+    return sum(a)

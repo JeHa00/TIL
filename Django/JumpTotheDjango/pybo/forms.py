59005255d02076b33db28dcb893098657acbaf7e
JeHa00 <rudtls0611@naver.com>
2022-08-16 22:20:16 +0900

    study

---
+++ Django/JumpTotheDjango/pybo/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from pybo.models import Question
+
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ['subject', 'content']
+        widgets = {
+            'subject': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
+        }
+        labels = {
+            'subject': '제목',
+            'content': '내용',
+        }  
--- Django/JumpTotheDjango/pybo/views.py
+++ Django/JumpTotheDjango/pybo/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
-
 from .models import Question
-
+from .forms import QuestionForm
 
 def index(request):
     question_list = Question.objects.order_by('-create_date')
@@ -21,3 +20,18 @@ def answer_create(request, question_id):
     question.answer_set.create(content=request.POST.get(
         'content'), create_date=timezone.now())
     return redirect('pybo:detail', question_id=question.id)
+
+
+def question_create(request): 
+    form = QuestionForm()
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit = False)
+            question.create_date = timezone.now() 
+            question.save() 
+            return redirect('pybo:index')
+        else:
+            form = QuestionForm() 
+        context = {'form': form}
+        return render(request, 'pybo/question_form.html', context)

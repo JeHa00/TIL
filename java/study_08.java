549233db2c0e8c2dc66497daca5f07a00e8e2f61
jeha00 <rudtls0611@naver.com>
2024-01-16 13:40:38 +0900

    JAVA: study

---
+++ java/study_08.java
@@ -0,0 +1,20 @@
+public class Scope01 {
+    public static void main(String[] args) {
+        int m = 10;
+        int temp = 0 ;
+        if (m > 0) {
+            temp = m * 2;
+            System.out.println(temp);
+        }
+        System.out.println(m);
+
+        // 개선
+        int m = 10;
+        if (m > 0) {
+            int temp = 2 * m;
+            System.out.println(temp);
+        }
+        System.out.println(m);
+    }
+}
+
---
+++ java/study_11.java
@@ -0,0 +1,14 @@
+public class Casting2 {
+    public static void main(String[] args) {
+        // 작은 범위의 형식 -> 큰 범위의 다른 형식으로 casting: 명시적 형변환
+        double doubleValue = 1.5;
+        int intValue = 0;
+
+        // intValue = doubleValue;
+
+        intValue = (int) doubleValue;
+        System.out.println(intValue);
+
+    }
+}
+
